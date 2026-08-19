8086a6a5ba409cab7d1f443d571b36c2ccdffacc
Obievousky Birgolini <acbracing@icloud.com>
2026-08-19 20:24:00 +0200

    Add startup warning for unwritable DATA_ROOT

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,28 @@
 #!/bin/sh
 set -e
 
-if [ "$1" = "process" ] || [ "$1" = "init-db" ] || [ "$1" = "serve" ]; then
+warn_data_root() {
+  data_dir="${DATA_ROOT:-/data}"
+  if [ ! -d "$data_dir" ]; then
+    echo "WARNING: DATA_ROOT '$data_dir' does not exist." >&2
+    echo "         Create and own it with: ./docker/provision.sh" >&2
+  elif [ ! -w "$data_dir" ]; then
+    echo "WARNING: DATA_ROOT '$data_dir' is not writable by uid $(id -u) (gid $(id -g))." >&2
+    echo "         Fix ownership with: ./docker/provision.sh" >&2
+  fi
+}
+
+if [ "$1" = "process" ]; then
+  warn_data_root
+  exec album-dvd-burner "$@"
+fi
+
+if [ "$1" = "init-db" ]; then
+  exec album-dvd-burner "$@"
+fi
+
+if [ "$1" = "serve" ]; then
+  warn_data_root
   exec album-dvd-burner "$@"
 fi
 
@@ -10,4 +31,5 @@ if [ "$1" = "--help" ] || [ "$1" = "-h" ]; then
 fi
 
 # Default: start web UI
+warn_data_root
 exec album-dvd-burner serve --host 0.0.0.0 --port "${WEB_PORT:-8080}"
